f0c50fcc5898eeabbb3975c33a76ee546da6dfe4
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-15 11:45:43 +0200

    remove unused test scripts, update main editor script

--- Assets/Scripts/Luny/Editor/MainEditorScript.lua
+++ Assets/Scripts/Luny/Editor/MainEditorScript.lua
@@ -1,13 +1,18 @@
 local context = ...
 
-print("context: " .. tostring(context))
-for k, v in pairs(context) do
-    print("\t" .. tostring(k) .. " = " .. tostring(v))
-end
+--print("context: " .. tostring(context))
+--for k, v in pairs(context) do
+--    print("\t" .. tostring(k) .. " = " .. tostring(v))
+--end
 
-print("Hello, MainEditorScript! script = " .. tostring(context))
+--print("Hello, MainEditorScript! script = " .. tostring(context))
 --dofile("New Folder/AuxEditorScript.lua")
 
-loadfile("New Folder/AuxEditorScript.lua", "t")(context)
+local auxScript = loadfile("New Folder/AuxEditorScript.lua", "t")
+if auxScript then
+    auxScript(context)
+else
+    print("auxScript is nil!")
+end
 
 --dofile("P:/test.lua")
