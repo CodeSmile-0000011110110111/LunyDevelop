a794dccad72a75d049c2aee76aceb35f248fbc89
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-18 12:22:34 +0200

    add script change counter

--- Assets/Scripts/Luny/Editor/MainEditorScript.lua
+++ Assets/Scripts/Luny/Editor/MainEditorScript.lua
@@ -1,5 +1,8 @@
 local context = ...
 
+context.ChangeCount = context.ChangeCount ~= nil and context.ChangeCount + 1 or 0
+print("Change count = " .. tostring(context.ChangeCount))
+
 --print("context: " .. tostring(context))
 --for k, v in pairs(context) do
 --    print("\t" .. tostring(k) .. " = " .. tostring(v))
