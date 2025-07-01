755cdc702ef3f05f6d8b959096dfb946640f48b2
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-07-01 18:14:05 +0200

    testing LuaTable inspector

--- Assets/Scripts/LuaSerializeTest.cs
+++ Assets/Scripts/LuaSerializeTest.cs
@@ -1,13 +1,26 @@
 using CodeSmile.Luny;
-using System;
+using UnityEditor;
 using UnityEngine;
 
 public sealed class LuaSerializeTest : MonoBehaviour
 {
-	public SerializableLuaValue serializedLuaValueTest;
+	public SerializedLuaValue LuaValueTest1;
+	public SerializedLuaValue LuaValueTest2;
+
+	public SerializedLuaKeyValue LuaKeyValueTest1;
+	public SerializedLuaKeyValue LuaKeyValueTest2;
+
+	public SerializedLuaTable LuaTableTest1;
+	public SerializedLuaTable LuaTableTest2;
 
 	private void OnValidate()
 	{
 		//Debug.Log("OnValidate: " + serializedLuaValueTest.ToString());
+
+		// Debug.Log("LuaTableTest1 dump:");
+		// foreach (var kvp in LuaTableTest1.Table)
+		// {
+		// 	Debug.Log($"\t[{kvp.Key}] = [{kvp.Value}] ({kvp.Value.Type})");
+		// }
 	}
 }
---
+++ Assets/bind-to-list/Editor/GameSwitchListEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UIToolkitExamples
+{
+    [CustomEditor(typeof(GameSwitchListAsset))]
+    public class GameSwitchListEditor : Editor
+    {
+        [SerializeField]
+        VisualTreeAsset m_ItemAsset;
+
+        [SerializeField]
+        VisualTreeAsset m_EditorAsset;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            m_EditorAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/bind-to-list/Editor/game_switch_list_editor.uxml");
+            m_ItemAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/bind-to-list/game_switch.uxml");
+            Debug.Log($"Assets: {m_EditorAsset}, {m_ItemAsset}");
+
+            var root = m_EditorAsset.CloneTree();
+            var listView = root.Q<ListView>();
+            listView.makeItem = m_ItemAsset.CloneTree;
+            return root;
+        }
+    }
+}
