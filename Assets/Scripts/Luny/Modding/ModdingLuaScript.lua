ca513182d3370a76ea6a165888f48b654fc13012
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-07-02 12:03:21 +0200

    inspector tests

--- Assets/Scripts/Luny/Modding/ModdingLuaScript.lua
+++ Assets/Scripts/Luny/Modding/ModdingLuaScript.lua
@@ -1,1 +1,4 @@
-print("Hello, Modding Lua script!")
+local context = ...
+print("Hello, Modding Lua script! " .. tostring(context) .. ", test = " .. tostring(context.test))
+
+context.test = 99999998
