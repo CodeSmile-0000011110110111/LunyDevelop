6905013272486e8eec3fce6c7deec22fda866e67
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-07-03 20:28:44 +0200

    test assets for LuaTable PropertyDrawer

--- Assets/Scripts/Luny/Modding/ModdingLuaScript.lua
+++ Assets/Scripts/Luny/Modding/ModdingLuaScript.lua
@@ -2,3 +2,22 @@ local context = ...
 print("Hello, Modding Lua script! " .. tostring(context) .. ", test = " .. tostring(context.test))
 
 context.test = 99999998
+if not context.updateCount then
+    context.updateCount = 0
+    context.fixedUpdateCount = 0
+end
+
+context.testTable =
+{
+    str = "Hello",
+    val = 1234.56789,
+    bool = true,
+}
+
+function context.Update()
+    context.updateCount = context.updateCount + 1
+end
+function context.FixedUpdate()
+    context.fixedUpdateCount = context.fixedUpdateCount + 1
+    --print(context.printInLua)
+end
