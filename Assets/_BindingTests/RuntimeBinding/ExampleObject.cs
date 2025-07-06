9f36f313c87fb5bbb4469aaa76d373c5638a5b45
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-07-06 11:32:42 +0200

    add another binding example

---
+++ Assets/_BindingTests/RuntimeBinding/ExampleObject.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (C) 2021-2025 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using UnityEngine;
+
+namespace _BindingTests.RuntimeBinding
+{
+	[CreateAssetMenu]
+	public class ExampleObject : ScriptableObject
+	{
+		[Header("Simple binding")]
+		public string simpleLabel = "Hello World!";
+	}
+}
