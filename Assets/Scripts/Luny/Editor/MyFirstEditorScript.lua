9d2d823186599a1e5ceaa642a1bcb8327c8e5616
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-07-11 19:36:22 +0200

    experiment with how to create enums in Lua

--- Assets/Scripts/Luny/Editor/MyFirstEditorScript.lua
+++ Assets/Scripts/Luny/Editor/MyFirstEditorScript.lua
@@ -5,13 +5,74 @@ local context = ...
 
 -- Use these events to respond to script loading, including hot reload
 function context.OnWillReloadScript()
-	print(context.ScriptName .. " was changed and will hot reload.")
+	--print(context.ScriptName .. " was changed and will hot reload.")
 end
 function context.OnDidLoadScript()
-	print(context.ScriptName .. " was (re-)loaded.")
+	--print(context.ScriptName .. " was (re-)loaded.")
 end
 
 -- Unity events are Lua functions of the same name, defined in the 'context' table:
 function context.OnEnable()
-	print("Hello, " .. context.ScriptName .. ".lua")
+	--print("Hello, " .. context.ScriptName .. ".lua")
 end
+
+
+print("---------------")
+local enumLength = #testEnum;
+print("length = " .. tostring(enumLength))
+print("testEnum = " .. tostring(testEnum) .. ", length = " .. tostring(#testEnum))
+
+for key, value in pairs(testEnum) do
+    print("\t" .. tostring(key) .. " = " .. tostring(value))
+end
+
+print("Fill = " .. tostring(testEnum.Fill))
+
+print("------...------")
+
+
+print("assign should raise error:")
+testEnum.Fill = 498
+
+
+--[[
+local enum =
+{
+    --values = {
+    --    123,
+    --    234,
+    --},
+    --names = {
+    --    First = 1,
+    --    Second = 2,
+    --    Third = 1,
+    --},
+}
+local lookup =
+{
+    First = 1,
+    Second = 2,
+    Third = 33,
+}
+local meta =
+{
+    values = {First = 3, Second = 4},
+    __index = function(t, k, v)
+        return lookup[k]
+        --return rawget(t.values, rawget(t.names, k))
+    end,
+    __newindex = function(t, k, v)
+        error("attempt to modify an enumeration", 2)
+    end,
+    __len = function(t)
+        return 3
+    end,
+}
+setmetatable(enum, meta)
+
+enum.First = 3
+print("First = " .. tostring(enum.First))
+print("Second = " .. tostring(enum.Second))
+print("Third = " .. tostring(enum.Third))
+print("value count = " .. tostring(#enum))
+]]
