7a292991db168a4b63aa2629dae0926aa1dce819
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-08-09 20:08:12 +0200

    update c# version of auto open scene assets

--- Assets/Scripts/Luny/Examples/Editor/AutoOpenCreatedSceneAssets.cs
+++ Assets/Scripts/Luny/Examples/Editor/AutoOpenCreatedSceneAssets.cs
@@ -9,6 +9,12 @@ public sealed class AutoOpenCreatedSceneAssets : AssetPostprocessor
 {
 	private static void OnPostprocessAllAssets(String[] importedPaths, String[] deletedPaths, String[] movedToPaths, String[] movedFromPaths)
 	{
+		if (importedPaths.Length > 0)
+			OnPostprocessImportedAssets(importedPaths);
+	}
+
+	private static void OnPostprocessImportedAssets(String[] importedPaths)
+	{
 		var scenePath = importedPaths.Where(path => Path.GetExtension(path).Equals(".unity")).FirstOrDefault();
 		if (scenePath != null)
 			EditorSceneManager.OpenScene(scenePath);
